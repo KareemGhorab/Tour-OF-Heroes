d7d6cbaa627e1610ab241a7cba19130f89447029
Kareem Ghorab <kareemghorab3@gmail.com>
2023-09-20 21:32:19 +0300

    [*] Logs store type fix

--- store/logs-store.ts
+++ store/logs-store.ts
@@ -1,7 +1,11 @@
 import { create } from 'zustand'
 let cur = 1
 
-const useLogs = create<{ logs: { id: number; log: string }[] }>((set) => ({
+const useLogs = create<{
+	logs: { id: number; log: string }[]
+	add: (log: string) => void
+	reset: () => void
+}>((set) => ({
 	logs: [],
 	add: (log: string) =>
 		set((state) => ({ logs: [...state.logs, { id: cur++, log }] })),
